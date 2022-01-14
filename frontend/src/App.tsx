8a3699ae149dfe39a0da4568ac65b21c2ccdd173
Isadora Firmo <isah3027@gmail.com>
2022-01-14 20:59:46 -0300

    _redirects

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,20 @@ import Listing from './pages/Listing';
 import Form from './pages/Form';
 import Navbar from "./components/Navbar";
 
+/*
+O React busca redirecionar o usuário para todas as rotas que contenham um determinado caminho.
+Por exemplo, se você cria uma rota /, ele direciona o usuário para todas as rotas com /
+É o que ta acontecendo ali.
+Ele acha que esse "/"form faz parte da rota "/"
+*/
+
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
+      <Routes> 
         <Route path="/" element={<Listing />} />
-        <Route path="/form">
+        <Route path="form">
           <Route path=":movieId" element={<Form />} />
         </Route>
       </Routes>
